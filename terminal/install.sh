65a9ac18c66304abb546686c07d2f71ba7b6a706
laxmikantm05 <laxmikantmaity1944@gmail.com>
2025-12-21 16:00:58 +0530

    Update install.sh

--- terminal/install.sh
+++ terminal/install.sh
@@ -29,5 +29,14 @@ chsh -s /bin/fish
 sleep 3
 
 sudo chsh -s /bin/fish
+sleep 1
 
-print "Voila !! your terminal has been set up. Go ahead and re-login..."
+print "Installing the Fonts"
+
+cd ..
+sleep 1
+cd fonts/
+sleep 1
+sudo cp -r * /usr/share/fonts
+
+print "Voila !! your terminal has been set up and your fonts have been installed. Go ahead and re-login..."
