137323bb3a038f667164b268d440c105b3eea161
laxmikantm05 <laxmikantmaity1944@gmail.com>
2025-12-21 16:41:02 +0530

    Update install.sh

--- terminal/install.sh
+++ terminal/install.sh
@@ -8,7 +8,7 @@ sudo dnf update
 
 sleep 1
 
-print "Installing Starship"
+echo "Installing Starship"
 sleep 3
 
 curl -sS https://starship.rs/install.sh | sh
@@ -42,4 +42,4 @@ cd fonts/
 sleep 1
 sudo cp -r * /usr/share/fonts
 
-print "Voila !! your terminal has been set up and your fonts have been installed. Go ahead and re-login..."
+echo "Voila !! your terminal has been set up and your fonts have been installed. Go ahead and re-login..."
