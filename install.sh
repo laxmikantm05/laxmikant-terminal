670290fd6a61f93f6cb0a61318b01364ea8cee98
Laxmikant <laxmikantmaity1944@gmail.com>
2026-04-15 12:14:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,25 @@
 #===========================
 # All-in-One install script
 #===========================
+
+sudo apt update -y
+sudo apt upgrade -y
+
+
+sudo apt install -y curl wget
+
+# Terminal Install Script:
+#==========================================
+bash ~/fancy-desktop/scripts/terminal.sh
+#==========================================
+
+# Fonts Install Script:
+#==========================================
+bash ~/fancy-desktop/scripts/fonts.sh
+#==========================================
+
+# my-apps Install Script:
+#==========================================
+bash ~/fancy-desktop/scripts/my-apps.sh
+#==========================================
+
